2e392c72fd7664b40c9509066f6efe197e252f51
Sebastian Försth <forsthsebbe@gmail.com>
2021-08-22 17:41:18 +0200

    RA-18 added some style to login screen

--- client/src/components/login/login.tsx
+++ client/src/components/login/login.tsx
@@ -44,24 +44,28 @@ const LoginComponent = () => {
 	};
 
 	return (
-		<div className='login-screen'>
+		<div className='login-screen grid'>
+			<div className="col-s-0 col-3"></div>
+			<div className="col-s-12 col-6 form-wrapper">
 			<form onSubmit={loginHandler} className='login-screen__form'>
 				<h3 className='login-screen__title'>Login</h3>
 				{error && <span className='error-message'>{error}</span>}
-				<div className='form-group'>
-					<label htmlFor='email'>Email:</label>
-					<input
-						type='email'
-						required
-						id='email'
-						placeholder='Email address'
-						onChange={(e) => setEmail(e.target.value)}
-						value={email}
-						tabIndex={1}
-					/>
+				<div className='form-group row'>
+					<label htmlFor='email' className="col-12">Email:</label>
+					<div className="email-input-wrapper col-12">
+							<input
+								type='email'
+								required
+								id='email'
+								placeholder='Email address'
+								onChange={(e) => setEmail(e.target.value)}
+								value={email}
+								tabIndex={1}
+							/>
+					</div>
 				</div>
-				<div className='form-group'>
-					<label htmlFor='password'>
+				<div className='form-group row'>
+					<label htmlFor='password' className="col-12">
 						Password:{' '}
 						<Link
 							to='/forgotpassword'
@@ -70,7 +74,9 @@ const LoginComponent = () => {
 							Forgot Password?
 						</Link>
 					</label>
+					<div className="password-input-wrapper col-12">
 					<input
+						className="col-6"
 						type='password'
 						required
 						id='password'
@@ -80,15 +86,21 @@ const LoginComponent = () => {
 						value={password}
 						tabIndex={2}
 					/>
+					</div>
 				</div>
-				<button type='submit' className='btn btn-primary'>
+				<div className="button-wrapper col-12">
+				<button type='submit' className='standard-button col-s-12 col-6 col-xl-3'>
 					Login
 				</button>
-
-				<span className='login-screen__subtext'>
+				</div>
+				<div className="register-wrapper row">
+				<span className='login-screen__subtext col-12'>
 					Don't have an account? <Link to='/register'>Register</Link>
 				</span>
+				</div>
 			</form>
+			</div>
+			<div className="col-s-0 col-3"></div>
 		</div>
 	);
 };
