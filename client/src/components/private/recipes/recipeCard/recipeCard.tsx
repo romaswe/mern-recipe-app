579920043da21f044b39f987a964d421ea745b4a
Sebastian Försth <forsthsebbe@gmail.com>
2021-08-29 21:22:31 +0200

    RA-27 testing jira automation

--- client/src/components/private/recipes/recipeCard/recipeCard.tsx
+++ client/src/components/private/recipes/recipeCard/recipeCard.tsx
@@ -3,8 +3,19 @@ import './recipeCard.css';
 
 export const RecipeCard = (props: any) => {
 	const recipe: Recipes = props.recipe;
+
+	const handleOnclick = () => {
+		console.log('clicked');
+		console.log(recipe.name);
+	};
+
 	return (
-		<div className='col-12 recipe-card-wrapper'>
+		<div
+			className='col-12 recipe-card-wrapper'
+			onClick={() => {
+				handleOnclick();
+			}}
+		>
 			<h3>{recipe.name}</h3>
 			<p>{recipe.description}</p>
 		</div>
