df1706f006a840f4c09172be38998076ee424bfb
Sebastian Försth <forsthsebbe@gmail.com>
2021-09-09 20:58:50 +0200

    RA-45 add single item from recipe to grocerylist

--- client/src/components/private/recipes/recipeCard/fullRecipe/fullRecipe.tsx
+++ client/src/components/private/recipes/recipeCard/fullRecipe/fullRecipe.tsx
@@ -5,12 +5,54 @@ import { Groceri, GrocerisObj } from '../../../../../entities/groceries';
 import { JwtData } from '../../../../../entities/jwt';
 import { Ingrediens, Recipes } from '../../../../../entities/recipes';
 import { isViewer } from '../../../../../utils/userUtils';
+import { GrAdd } from 'react-icons/gr';
 import './fullRecipe.css';
 
 export const FullRecipe = (props: any) => {
 	const recipe: Recipes = props.recipe;
 	const [error, setError] = useState('');
 	const [success, setSuccess] = useState('');
+
+	const addToGrocerieList = async (ingredient: Ingrediens) => {
+		var token = localStorage.getItem('authToken') ?? '';
+		const jwtData: JwtData = jwt_decode(token);
+		setError('');
+		if (isViewer(jwtData)) {
+			alert("Du kan inte använda denna funktion, läs mer under 'om'");
+			return;
+		}
+
+		try {
+			const config = {
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${localStorage.getItem(
+						'authToken'
+					)}`,
+				},
+			};
+			const grocerie: GrocerisObj = {
+				name: 'MyList',
+				groceries: [
+					{
+						name: `${ingredient.amount}${ingredient.unit} ${ingredient.name}`,
+						amount: 1,
+					},
+				],
+			};
+			await axios.post('/api/private/groceries', grocerie, config);
+			setSuccess('Ingrediens tillagd');
+			setTimeout(() => {
+				setSuccess('');
+			}, 5000);
+		} catch (error) {
+			console.log(error);
+			setError(error?.response?.data.error);
+			setTimeout(() => {
+				setError('');
+			}, 5000);
+		}
+	};
 
 	const handleAddClick = async () => {
 		var token = localStorage.getItem('authToken') ?? '';
@@ -109,14 +151,26 @@ export const FullRecipe = (props: any) => {
 								return (
 									<div className='col-12' key={i}>
 										<div className='row ingredent-row'>
-											<div className='col-s-3 col-3'>
+											<div className='col-3'>
 												<p>
 													{ingredient.amount}{' '}
 													{ingredient.unit}
 												</p>
 											</div>
-											<div className='col-s-9 col-9'>
+											<div className='col-6'>
 												<p>{ingredient.name}</p>
+											</div>
+											<div className='col-3 add-grocerie-recipe-button-wrapper'>
+												<button
+													className='add-grocerie-recipe-button'
+													onClick={() =>
+														addToGrocerieList(
+															ingredient
+														)
+													}
+												>
+													<GrAdd />
+												</button>
 											</div>
 										</div>
 									</div>
@@ -133,7 +187,7 @@ export const FullRecipe = (props: any) => {
 				</div>
 
 				<button className='submit-button' onClick={handleAddClick}>
-					Lägg till inköpslistan
+					Lägg allt i inköpslistan
 				</button>
 			</div>
 			<div className='col-6'>
