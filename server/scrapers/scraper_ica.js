7c4bc1f287d5258e81c163c3e87975f9c1349db4
Sebastian <forsthsebbe@gmail.com>
2024-06-22 13:13:22 +0200

    Added scraper to scrape recipes from ica

---
+++ server/scrapers/scraper_ica.js
@@ -0,0 +1,110 @@
+import { JSDOM } from 'jsdom';
+
+async function scrapeIcaRecipe(url) {
+	try {
+		const response = await fetch(url);
+		const data = await response.text();
+		const dom = new JSDOM(data);
+		const document = dom.window.document;
+
+		const name =
+			document
+				.querySelector('h1.recipe-header__title')
+				?.textContent.trim() || '';
+
+		const notesArray = Array.from(
+			document.querySelectorAll('div.ingredients-list-group__card__ingr')
+		).map((el) => el.textContent.trim());
+		const notes = notesArray.join(' ');
+
+		const description =
+			document
+				.querySelector('div.recipe-header__preamble p')
+				?.textContent.trim() || '';
+
+		const categories = Array.from(
+			document.querySelectorAll('div.more-like-this__categories a')
+		).map((el) => el.textContent.trim());
+
+		const ingrediens = Array.from(
+			document.querySelectorAll('div.ingredients-list-group__card')
+		)
+			.map((el) => {
+				const name =
+					el
+						.querySelector(
+							'span.ingredients-list-group__card__ingr'
+						)
+						?.textContent.trim() || '';
+				let amount =
+					el
+						.querySelector('span.ingredients-list-group__card__qty')
+						?.textContent.trim() || '';
+				let unit = ''; // Initialize unit
+				if (amount.match(/^\d+(\s*-\s*\d*)?$/)) {
+					// TODO: this unit should me localized when thats implemented
+					unit = 'st'; // Set unit to 'st' if amount is just a number or number with a dash
+				} else {
+					const match = amount.match(/^([\d\s\/.-]+)(.*)$/);
+					if (match) {
+						amount = match[1].trim();
+						unit = match[2].trim();
+					}
+				}
+				return { name, amount, unit };
+			})
+			.filter(
+				(ingredient) =>
+					ingredient.name || ingredient.amount || ingredient.unit
+			);
+
+		const instructions = Array.from(
+			document.querySelectorAll('div.cooking-steps-card')
+		).map((el) => {
+			const step =
+				el
+					.querySelector('div.cooking-steps-main__text')
+					?.textContent.trim() || '';
+
+			return step;
+		});
+
+		const tags = Array.from(
+			document.querySelectorAll('div.tags__item')
+		).map((el) => el.textContent.trim());
+
+		const recipe = {
+			name,
+			url,
+			notes,
+			description,
+			categories,
+			ingrediens,
+			instructions,
+			tags,
+		};
+
+		return recipe;
+	} catch (error) {
+		console.error('Error scraping recipe:', error);
+	}
+}
+
+export default {
+	scrapeIcaRecipe,
+};
+
+/* 
+Usage:
+const { scrapeIcaRecipe } = require('./scraper');
+
+const recipeUrl = 'https://www.ica.se/recept/vitloksrostad-farskpotatis-med-spenat-713530/';
+scrapeIcaRecipe(recipeUrl).then(recipe => {
+    if (recipe) {
+        console.log(recipe);
+    } else {
+        console.error('Failed to scrape the recipe.');
+    }
+});
+
+*/
